c91593902ea5d08e7109967f29467ee10260157c
kyomexd <118078007+kyomexd@users.noreply.github.com>
2022-11-13 01:22:21 +0300

    Update FinancialCalc.java

--- Making The Cut/src/TestTaskForMentoring/FinancialCalc.java
+++ Making The Cut/src/TestTaskForMentoring/FinancialCalc.java
@@ -10,24 +10,21 @@ public class FinancialCalc {
                 Tester inputData = new Tester();
                 Calculus.input = Tester.input;
                 Calculus answered = new Calculus();
-                if (inputData.ifPositive()) {
-                        if (inputData.ifCalculable()) {
+                if (inputData.ifPositiveSumCred()) {
+                        if (inputData.ifPositiveMonPay()) {
+                                if (inputData.ifPositiveIR()) {
+                                        if (inputData.ifCorrectHumanBus()) {
 
-                                if (inputData.HorB()) {
-                                        double result = Math.floor(answered.calcForHuman());
-                                        System.out.println(result);
-                                } else {
-                                        double result = Math.floor(answered.calcForBusiness());
-                                        System.out.println(result);
-                                }
-                        }
-                        else {
-                                throw new ArithmeticException ("Input incalculable");
-                        }
-                }
-                else {
-                        throw new ArithmeticException("Wrong input");
-                }
+
+                                                if (inputData.ifCalculable()) {
+                                                        double result = Math.floor(answered.calc());
+                                                        System.out.println(result);
+
+                                                } else throw new ArithmeticException("Input incalculable");
+                                        } else throw new ArithmeticException("This software only supports human or business as clients");
+                                } else throw new ArithmeticException("Interest Rate input is incorrect");
+                        } else throw new ArithmeticException("Monthly payment input is incorrect");
+                } else throw new ArithmeticException("Sum of debit input is incorrect");
 
 
         }
