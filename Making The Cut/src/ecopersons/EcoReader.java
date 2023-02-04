17a01090ed517fdfa1aed6d83064e931edcb6284
kyomexd <118078007+kyomexd@users.noreply.github.com>
2023-02-04 17:04:26 +0300

    Delete Making The Cut/src directory

--- Making The Cut/src/ecopersons/EcoReader.java
+++
@@ -1,9 +0,0 @@
-package ecopersons;
-
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.List;
-
-public interface EcoReader {
-    List<Person> readFromFile(Path path) throws IOException;
-}
