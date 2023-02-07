9d33960aafc9732bdadcade82e012696b94e44c7
kyomexd <118078007+kyomexd@users.noreply.github.com>
2023-02-07 23:18:30 +0300

    Update Counter.java

--- AdditionalTasks/src/expressionValidator/Counter.java
+++ AdditionalTasks/src/expressionValidator/Counter.java
@@ -1,33 +1,38 @@
 package expressionValidator;
 
+import java.util.*;
+import java.util.function.BiPredicate;
+
 public class Counter {
 
     protected static int result(String expression) {
-        int result = 0;
-        char valueAt;
-        StringBuilder currentValue = new StringBuilder();
-        StringBuilder firstValue = new StringBuilder();
-        Operations operation = Operations.NONE;
-        for (int i = 0; i < expression.length(); i++) {
-            valueAt = expression.charAt(i);
-            if (Character.isDigit(valueAt)) {
-                currentValue.append(valueAt);
+        char[] tokens = expression.toCharArray();
+        Deque<Integer> values = new ArrayDeque<>();
+        Deque<Character> operations = new ArrayDeque<>();
+
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i] == ' ')
+                continue;
+            if (Character.isDigit(tokens[i])) {
+                StringBuilder stringBuilder = new StringBuilder();
+                while (i < tokens.length && Character.isDigit(tokens[i])) {
+                    stringBuilder.append(tokens[i++]);
+                }
+                values.push(Integer.parseInt(stringBuilder.toString()));
+                i--;
             }
-            if (valueAt == '+' || valueAt == '-' || valueAt == '*' || i == expression.length() - 1) {
-                result += operation.computeMath(firstValue.toString(), currentValue.toString());
-                if (valueAt == '+') {
-                    operation = Operations.ADDITION;
+            else if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*') {
+                while (!operations.isEmpty() && hasPrecedence.test(tokens[i], operations.peek())) {
+                    values.push(Operations.applyOp(operations.pop(), values.pop(), values.pop()));
                 }
-                if (valueAt == '-') {
-                    operation = Operations.SUBTRACTION;
-                }
-                if (valueAt == '*') {
-                    operation = Operations.MULTIPLICATION;
-                }
-                firstValue = currentValue;
-                currentValue = new StringBuilder();
+                operations.push(tokens[i]);
             }
         }
-        return result;
+        while (!operations.isEmpty())
+            values.push(Operations.applyOp(operations.pop(), values.pop(), values.pop()));
+        return values.pop();
     }
+
+    public static BiPredicate<Character, Character> hasPrecedence = (op1, op2) ->
+            (op1 != '*' && op1 != '/') || (op2 != '+' && op2 != '-');
 }
