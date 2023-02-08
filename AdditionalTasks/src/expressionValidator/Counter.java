25a263d2f733b6a0343f919ec83a13fa9e2e1df6
kyomexd <118078007+kyomexd@users.noreply.github.com>
2023-02-08 17:42:05 +0300

    Update Counter.java

--- AdditionalTasks/src/expressionValidator/Counter.java
+++ AdditionalTasks/src/expressionValidator/Counter.java
@@ -1,38 +1,29 @@
 package expressionValidator;
 
-import java.util.*;
-import java.util.function.BiPredicate;
-
 public class Counter {
 
     protected static int result(String expression) {
-        char[] tokens = expression.toCharArray();
-        Deque<Integer> values = new ArrayDeque<>();
-        Deque<Character> operations = new ArrayDeque<>();
-
-        for (int i = 0; i < tokens.length; i++) {
-            if (tokens[i] == ' ')
-                continue;
-            if (Character.isDigit(tokens[i])) {
-                StringBuilder stringBuilder = new StringBuilder();
-                while (i < tokens.length && Character.isDigit(tokens[i])) {
-                    stringBuilder.append(tokens[i++]);
-                }
-                values.push(Integer.parseInt(stringBuilder.toString()));
-                i--;
-            }
-            else if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*') {
-                while (!operations.isEmpty() && hasPrecedence.test(tokens[i], operations.peek())) {
-                    values.push(Operations.applyOp(operations.pop(), values.pop(), values.pop()));
-                }
-                operations.push(tokens[i]);
-            }
+        expression = expression.replaceAll(" ", "");
+        if (assertPriority(expression) == 2) {
+            Multiplication multiplication = new Multiplication();
+            expression = multiplication.solveExpressions(expression);
         }
-        while (!operations.isEmpty())
-            values.push(Operations.applyOp(operations.pop(), values.pop(), values.pop()));
-        return values.pop();
+        if (assertPriority(expression) == 1) {
+            Addition addition = new Addition();
+            Subtraction subtraction = new Subtraction();
+            expression = addition.calculate(expression);
+            expression = subtraction.calculate(expression);
+        }
+        return Integer.parseInt(expression);
     }
 
-    public static BiPredicate<Character, Character> hasPrecedence = (op1, op2) ->
-            (op1 != '*' && op1 != '/') || (op2 != '+' && op2 != '-');
+    private static int assertPriority(String expression) {
+        if (expression.contains("*")) {
+            return 2;
+        }
+        if (expression.contains("+") || expression.contains("-")) {
+            return 1;
+        }
+        return 0;
+    }
 }
